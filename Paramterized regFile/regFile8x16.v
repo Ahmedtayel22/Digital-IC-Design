6b5b22b43973de3f5d8ae3fe912744b75b0a3c93
Ahmed Tayel <105231666+Ahmedtayel22@users.noreply.github.com>
2023-09-23 22:24:08 +0300

    regFile8x16.v

--- Paramterized regFile/regFile8x16.v
+++ Paramterized regFile/regFile8x16.v
@@ -3,9 +3,9 @@
 /////////////////////// regFile8x16 /////////////////////////
 /////////////////////////////////////////////////////////////
 
-module regFile8x16 #(parameter width = 8, depth = 16, addressBus = 4)
+module regFile8x16 #(parameter width = 16, depth = 8, addressBus = 4)
     (WrData, Address, WrEN, RdEN, REF_CLK, RST, 
-    RdData, RdData_Valid, REG0, REG1, REG2, REG3);
+    RdData, RdData_Valid);
     input [width-1:0] WrData;
     input [addressBus-1:0] Address;
     input WrEN;
@@ -14,37 +14,22 @@ module regFile8x16 #(parameter width = 8, depth = 16, addressBus = 4)
     input RST;
     output [width-1:0] RdData;
     output RdData_Valid;
-    output wire  [width-1:0] REG0;
-    output wire  [width-1:0] REG1;
-    output wire  [width-1:0] REG2;
-    output wire  [width-1:0] REG3;
 
     reg RdData_Valid;
     reg [width-1:0] RdData;
-    reg [width-1:0] reg_file [0:depth-1]; //2D-array memory 
-    assign REG0 = reg_file[0];
-    assign REG1 = reg_file[1];
-    assign REG2 = reg_file[2];
-    assign REG3 = reg_file[3];
+    reg [width-1:0] reg_file [0:depth-1]; 
     integer i;
-
-
 
     always@(posedge REF_CLK or negedge RST)
     begin
         if(!RST)
         begin
-            for (i=4; i<depth; i=i+1)
+            for (i=0; i<depth; i=i+1)
             begin
                  reg_file[i]<= 0;
             end
-            reg_file [0] <= 'b0;
-            reg_file [1] <= 'b0;
-            reg_file [2] <= 'b10000001;
-            reg_file [3] <= 'b00100000;
             RdData       <= 0;
             RdData_Valid <= 0;
-
         end
         else if (WrEN ==1 && RdEN==0)
         begin
