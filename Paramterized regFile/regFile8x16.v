eda53b620727195de06b888831fab8a78efbbc67
Ahmed Tayel <105231666+Ahmedtayel22@users.noreply.github.com>
2023-09-23 22:26:01 +0300

    regFile8x16.v

--- Paramterized regFile/regFile8x16.v
+++ Paramterized regFile/regFile8x16.v
@@ -2,9 +2,8 @@
 /////////////////////////////////////////////////////////////
 /////////////////////// regFile8x16 /////////////////////////
 /////////////////////////////////////////////////////////////
-
-module regFile8x16 #(parameter width = 16, depth = 8, addressBus = 3)
-    (wr_data, address,wr_en, rd_en, CLK, RST, rd_data);
+module regFile8x16 #(parameter width = 16, depth = 8, addressBus = 4)
+    (wr_data, address, wr_en, rd_en, CLK, RST, rd_data);
     input [width-1:0] wr_data;
     input [addressBus-1:0] address;
     input wr_en;
