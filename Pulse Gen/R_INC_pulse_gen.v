ddd1a6caadf8d47b344a7ce65beded882c5f97a7
Ahmed Tayel <105231666+Ahmedtayel22@users.noreply.github.com>
2023-09-24 15:51:28 +0300

    R_INC_pulse_gen.v

--- Pulse Gen/R_INC_pulse_gen.v
+++ Pulse Gen/R_INC_pulse_gen.v
@@ -1,13 +1,13 @@
 
 /////////////////////////////////////////////////////////////
-///////////////////// R_INC_pulse_gen ///////////////////////
+///////////////////// pulse_gen ///////////////////////
 /////////////////////////////////////////////////////////////
 
-module R_INC_pulse_gen (IN, R_INC_pulse, CLK, RST);
+module pulse_gen (IN, pulse, CLK, RST);
 input IN;
 input CLK;
 input RST;
-output wire R_INC_pulse;
+output wire pulse;
 reg IN_ff;
 
 
@@ -15,7 +15,7 @@ wire IN_and_0;
 wire IN_and_1;
 assign IN_and_0 = ~ (IN_ff);
 assign IN_and_1 = IN;
-assign R_INC_pulse = IN_and_0 & IN_and_1;
+assign pulse = IN_and_0 & IN_and_1;
 
 
 always @ (posedge CLK or negedge RST)
